eae370695cec343a9e54306b0705d39dcaa82229
Diego Czel Stepanha <diegoczel@gmail.com>
2023-01-28 07:04:51 -0300

    Start study of RestSharp

--- src/Domain/Models/Produto.cs
+++ src/Domain/Models/Produto.cs
@@ -2,6 +2,15 @@
 
 public class Produto : Entity
 {
+    public Produto() { }
+
+    public Produto(string nome, string complemento, EProdutoTipo produtoTipo) 
+    {
+        Nome = nome;
+        Complemento = complemento;
+        ProdutoTipo = produtoTipo;
+    }
+
     public string Nome { get; set; } = String.Empty;
 
     public string Complemento { get; set; } = String.Empty;
---
+++ test/Api.Console.RestSharpTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using RestSharp;
+
+
+await TestGet("produto", 29);
+await TestPost("produto", new Produto("Pizza", "Pizza", 0));
+
+static async Task TestGet(string rota, int id)
+{
+    var request = new RestRequest($"{rota}/{id}");
+    var response = await _client.GetAsync(request);
+    Console.WriteLine(response.Content);
+}
+
+static async Task TestPost(string rota, Produto p)
+{
+    var request = new RestRequest($"{rota}", Method.Post)
+        .AddJsonBody(p);
+
+    var response = await _client.PostAsync(request);
+
+    if(response.IsSuccessful)
+        Console.WriteLine(response.Content);
+}
+
+partial class Program
+{
+    private static RestClient _client = new RestClient("https://localhost:7024");
+}
